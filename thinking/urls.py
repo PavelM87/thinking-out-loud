a7840ab687fb0c1a7e58e65a0942e6a0ffa74763
PavelM87 <mospv87@gmail.com>
2021-06-28 22:32:38 +0300

    Added models, views and templates

--- thinking/urls.py
+++ thinking/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from thoughts.views import home, post_detail
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('posts/<int:post_id>', post_detail),
 ]
--- thoughts/views.py
+++ thoughts/views.py
@@ -1,3 +1,24 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
-# Create your views here.
+from .models import Post
+
+
+def home(request, *args, **kwargs):
+    return render(request, 'pages/home.html', context={}, status=200)
+
+def post_detail(request, post_id, *args, **kwargs):
+
+    data = {
+        "id": post_id,
+    }
+    status = 200
+    try:
+        obj = Post.objects.get(id=post_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+
+    return JsonResponse(data, status=status)
