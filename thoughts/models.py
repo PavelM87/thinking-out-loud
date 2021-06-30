9e3b5ec2d3d2e19d6fb866706fe8c42b8186ef96
PavelM87 <mospv87@gmail.com>
2021-07-01 00:01:10 +0300

    Set up admin panel

--- thoughts/models.py
+++ thoughts/models.py
@@ -1,11 +1,19 @@
 import random
 
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 
 class Post(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+
+    # def __str__(self):
+    #     return self.content
 
     class Meta:
         ordering = ['-id']
