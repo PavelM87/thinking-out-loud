9d99e9f0ab5ec36fe2be5c7098b20d2c4293fde9
PavelM87 <mospv87@gmail.com>
2021-07-03 12:11:27 +0300

    DRF permissions

--- thinking/settings.py
+++ thinking/settings.py
@@ -130,3 +130,13 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MAX_POST_LENGTH = 240
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication'
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer'
+    ]
+}
--- thoughts/views.py
+++ thoughts/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, JsonResponse
 from django.utils.http import is_safe_url
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 
 from .models import Post
 from .forms import PostForm
@@ -18,6 +20,8 @@ def home(request, *args, **kwargs):
     return render(request, 'pages/home.html', context={}, status=200)
 
 @api_view(['POST'])
+# @authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
 def post_create(request, *args, **kwargs):
     serializer = PostSerializer(data=request.POST)
     if serializer.is_valid(raise_exception=True):
