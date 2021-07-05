0ffcaa62ab7581e522a7644353642b2595253c41
PavelM87 <mospv87@gmail.com>
2021-07-05 20:51:13 +0300

    React-app setting. Displaying posts

--- thinking-web/src/App.js
+++ thinking-web/src/App.js
@@ -1,7 +1,49 @@
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function loadPosts(callback) {
+  const xhr = new XMLHttpRequest()
+  const method = 'GET'
+  const url = 'http://localhost:8000/api/posts'
+  const responseType = 'json'
+
+  xhr.responseType = responseType
+  xhr.open(method, url)
+  xhr.onload = function() {
+    callback(xhr.response, xhr.status )
+  }
+  xhr.onerror = function (e) {
+    console.log(e)
+    callback({"message": "The request was an error"}, 400)
+  }
+  xhr.send()
+}
+
+function Post(props) {
+  const {post} = props
+  const className = props.className ? props.className : 'col-10 mx-auto, col-md-6'
+  return <div className={className}>
+    <p>{post.content}</p>
+  </div>
+}
+
 function App() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const myCallback = (response, status) => {
+      console.log(response, status)
+      if (status === 200){
+        setPosts(response)
+      } else {
+        alert("Ошибка же")
+      }
+    }
+    loadPosts(myCallback)
+    }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +51,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          {posts.map((post, index)=>{
+            return <Post post={post} className='my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`}/>
+          })}
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
--- thinking/settings.py
+++ thinking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ekijz3ymntlhjl989tny45=)=qba%i$%#cka-afpon3*ugfn&m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 LOGIN_URL = "/login"
 
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'thoughts',
 ]
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,6 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MAX_POST_LENGTH = 240
 POST_ACTION_OPTIONS = ["like", "unlike", "repost"]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 DEFAULT_RENDERER_CLASSES = [
     'rest_framework.renderers.JSONRenderer',
