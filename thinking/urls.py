580aeeefaf9ea1c44494dafbee4f391cc1fd14fa
PavelM87 <mospv87@gmail.com>
2021-07-07 00:44:22 +0300

    React Rendered by Django

--- thinking/urls.py
+++ thinking/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from thoughts.views import (
     home, post_detail, post_list,
@@ -24,10 +27,12 @@ from thoughts.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('react/', TemplateView.as_view(template_name='react.html')),
     path('create-post', post_create),
     path('posts', post_list),
     path('posts/<int:post_id>', post_detail),
     path('api/posts/', include('thoughts.urls')),
-    # path('api/posts/action', post_action),
-    # path('api/posts/<int:post_id>/delete', post_delete),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
