5dc2c82451979cd3049770b9ae87ebb19146c1eb
PavelM87 <mospv87@gmail.com>
2021-07-16 23:30:39 +0300

    Profiles. Form & model

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import profile_detail
+from .views import profile_detail, profile_update
 
 
 urlpatterns = [
+    path('edit', profile_update),
     path('<str:username>', profile_detail),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,37 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .models import Profile
+from .forms import ProfileForm
+
+
+def profile_update(request, *args, **kwargs):
+    if not request.user.is_authenticated:
+        return redirect("/login?next=/profile/update")
+    user = request.user
+    user_data = {
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "email": user.email,
+    }
+    my_profile = request.user.profile
+    form = ProfileForm(request.POST or None, instance=my_profile, initial=user_data)
+    if form.is_valid():
+        profile_obj = form.save(commit=False)
+        first_name = form.cleaned_data.get('first_name')
+        last_name = form.cleaned_data.get('last_name')
+        email_address = form.cleaned_data.get('email')
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email_address = email_address
+        user.save()
+        profile_obj.save()
+    context = {
+        "form": form,
+        "btn_label": "Сохранить",
+        "title": "Редактировать профиль",
+    }
+    return render(request, "profiles/form.html", context)
 
 
 def profile_detail(request, username, *args, **kwargs):
