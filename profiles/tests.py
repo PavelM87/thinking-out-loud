54e559fcb5d9de646b7f3e53b7831bf021a035d9
PavelM87 <mospv87@gmail.com>
2021-07-18 00:35:04 +0300

    Added FollowerRelation and tests

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from .models import Profile
+
+User = get_user_model()
+
+class ProfileTestCase(TestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username="qwe", password="strong123")
+        self.user_1 = User.objects.create_user(username="rty", password="strong1234")
+
+    def test_profile_created_via_signal(self):
+        qs = Profile.objects.all()
+        self.assertEqual(qs.count(), 2)
+
+    def test_following(self):
+        first = self.user
+        second = self.user_1
+        first.profile.followers.add(second) # добавляю подписчика
+        second_user_following_whom = second.following.all()
+        qs = second_user_following_whom.filter(user=first) # проверяю что подписчик появился
+        self.assertTrue(qs.exists())
+
+        first_user_following_no_one = first.following.all() # проверяю что новый пользователь ни на кого не подписан
+        self.assertFalse(first_user_following_no_one.exists())
